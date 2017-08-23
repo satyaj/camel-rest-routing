57c64a1dd668231ce770a4eb8341c703e66f4c30
Satya Jayanti <sjayanti@redhat.com>
2017-08-23 16:34:54 +0530

    Initial check in of REST-SOAP Proxy route

---
+++ src/main/java/com/redhat/gpe/training/JSONResponse.java
@@ -0,0 +1,21 @@
+package com.redhat.gpe.training;
+
+
+public class JSONResponse {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public JSONResponse(String message) {
+		super();
+		this.message = message;
+	}
+	
+}
